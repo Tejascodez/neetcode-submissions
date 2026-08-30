4f64047ab5296e6094d415491cfd594b5ca5f130
Tejas Namdev Patil <156433908+Tejascodez@users.noreply.github.com>
2026-08-30 23:46:07 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        String parts[] = path.split("/");
+
+        for(String part : parts){
+            if(part.equals("") || part.equals(".")) continue;
+
+            if(part.equals("..")){
+                if(!st.isEmpty()) st.pop();
+            }else{
+                st.push(part);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(String dir : st){
+            result.append("/").append(dir);
+        }
+
+        if(result.length
+        
+        () == 0) return "/";
+
+        return result.toString();
+    }
+}
