c1e99b2479edb7284b9a57986b96478392ec81da
Tejas Namdev Patil <156433908+Tejascodez@users.noreply.github.com>
2026-09-04 23:02:10 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solve(int n, int memo[]){
+        if(n <= 2) return n;
+        if(memo[n] != -1) return memo[n];
+
+        memo[n] = solve(n-1,memo) + solve(n-2,memo);
+        return memo[n];
+
+    }
+    public int climbStairs(int n) {
+        int memo[] = new int[n+1];
+        Arrays.fill(memo,-1);
+
+        return solve(n,memo);
+    }
+}
